ada0702fce14f2e3126218299276043fe8c01d37
tama <tama@ttk1.net>
2019-05-31 01:14:25 +0900

    グローバル変数にモードを持つようにした。

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ const main = () => {
     const container = document.getElementById('container');
     const canvas = document.createElement('canvas');
     container.appendChild(canvas);
-
-    if (container.classList.contains('randomlines')) {
-        return require('./randomlines.js').start(canvas);
-    }
-    if (container.classList.contains('animation01')) {
-        return require('./animation01.js').start(canvas);
+    
+    switch (mode) {
+        case 'randomlines':
+            return require('./randomlines.js').start(canvas);
+        case 'animation01':
+            return require('./animation01.js').start(canvas);
     }
 }
 
