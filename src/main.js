0c93db15a79e7709cfb359e8cc99e60935fa158f
tama <tama@ttk1.net>
2019-05-31 01:34:44 +0900

    グローバル変数からクエリパラメータに変更。

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ const main = () => {
     const container = document.getElementById('container');
     const canvas = document.createElement('canvas');
     container.appendChild(canvas);
-    
+
+    const mode = getParam('mode');
     switch (mode) {
         case 'randomlines':
             return require('./randomlines.js').start(canvas);
         case 'animation01':
             return require('./animation01.js').start(canvas);
     }
+}
+
+function getParam(key) {
+    return window.location.search
+        .replace(/^\?/, '').split('&')
+        .map(x => x.split('='))
+        .find(x => x[0] == key)[1];
 }
 
 window.onload = main;
