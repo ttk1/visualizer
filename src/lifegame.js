d7f998951f17821c6642f9ec28f75514206e94dd
tama <tama@ttk1.net>
2019-06-03 01:01:36 +0900

    start, stop, resetをFieldクラスのメソッドに持って行った。

--- src/lifegame.js
+++ src/lifegame.js
@@ -2,30 +2,32 @@ const field_width = 100;
 const field_height = 100;
 const cell_size = 7;
 const interval = 100;
-let interval_id = null;
 
 class Field {
-    constructor(width, height, canvas) {
+    constructor(width, height, cell_size, canvas) {
         this.step_num = 0;
         this.width = width;
         this.height = height;
+        this.cell_size = cell_size;
         this.canvas = canvas;
-        this.canvas.width = field_width * cell_size;
-        this.canvas.height = field_height * cell_size;
+        this.canvas.width = width * cell_size;
+        this.canvas.height = height * cell_size;
         this.canvas.style.border = 'solid 1px black';
         this.ctx = this.canvas.getContext('2d');
         this.field = [];
         this.reset();
+        this.interval = interval;
+        this.interval_id = null;
 
         this.canvas.addEventListener('click', (e) => {
-            if (interval_id != null) {
+            if (this.interval_id != null) {
                 return;
             }
 
             let rect = e.target.getBoundingClientRect();
             let x = Math.floor((e.clientX - rect.left) / cell_size);
             let y = Math.floor((e.clientY - rect.top) / cell_size);
-            if (this.get(x ,y)) {
+            if (this.get(x, y)) {
                 this.unfill(x, y);
             } else {
                 this.fill(x, y);
@@ -89,9 +91,33 @@ class Field {
         this.field[x][y] = !this.field[x][y];
     }
 
+    start() {
+        if (this.interval_id == null) {
+            this.interval_id = window.setInterval(() => {
+                this.step();
+                this.refresh();
+                for (let x = 0; x < this.width; x++) {
+                    for (let y = 0; y < this.height; y++) {
+                        if (this.get(x, y)) {
+                            this.fill(x, y);
+                        }
+                    }
+                }
+            }, this.interval);
+        }
+    }
+
+    stop() {
+        if (this.interval_id != null) {
+            window.clearInterval(this.interval_id)
+            this.interval_id = null;
+        }
+    }
+
     reset() {
-        this.step_num = 0;
+        this.stop();
         this.refresh();
+        this.step_num = 0;
         for (let x = 0; x < this.width; x++) {
             this.field[x] = [];
             for (let y = 0; y < this.height; y++) {
@@ -109,20 +135,20 @@ class Field {
     fill(x, y) {
         this.ctx.beginPath();
         this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+        this.ctx.fillRect(x * this.cell_size, y * this.cell_size, this.cell_size, this.cell_size);
     }
 
     unfill(x, y) {
         this.ctx.beginPath();
         this.ctx.fillStyle = 'white';
-        this.ctx.fillRect(x * cell_size, y * cell_size, cell_size, cell_size);
+        this.ctx.fillRect(x * this.cell_size, y * this.cell_size, this.cell_size, this.cell_size);
     }
 }
 
 function init_button(field) {
     const container = document.getElementById('container');
     const button_area = document.createElement('div');
-    button_area.style.display="block";
+    button_area.style.display = "block";
     container.appendChild(button_area);
 
     const start_button = document.createElement('button');
@@ -136,39 +162,12 @@ function init_button(field) {
     button_area.appendChild(stop_button);
     button_area.appendChild(reset_button);
 
-    start_button.addEventListener('click', () => {
-        if (interval_id == null) {
-            interval_id = window.setInterval(animate, interval);
-        }
-
-        function animate() {
-            field.step();
-            field.refresh();
-            for (let x = 0; x < field_width; x++) {
-                for (let y = 0; y < field_height; y++) {
-                    if (field.get(x, y)) {
-                        field.fill(x, y);
-                    }
-                }
-            }
-        }
-    });
-    stop_button.addEventListener('click', () => {
-        if (interval_id != null) {
-            window.clearInterval(interval_id)
-            interval_id = null;
-        }
-    });
-    reset_button.addEventListener('click', () => {
-        if (interval_id != null) {
-            window.clearInterval(interval_id)
-            interval_id = null;
-        }
-        field.reset();
-    });
+    start_button.addEventListener('click', () => field.start());
+    stop_button.addEventListener('click', () => field.stop());
+    reset_button.addEventListener('click', () => field.reset());
 }
 
 exports.start = (canvas) => {
-    const field = new Field(field_width, field_height, canvas);
+    const field = new Field(field_width, field_height, cell_size, canvas);
     init_button(field);
 }
